7701faff33ba4695a5ce60455815fac0495bed71
gauravkumar_singh1 <gauravkumar_singh1@epam.com>
2025-03-06 11:04:47 +0530

    init

--- task01/jsrc/main/java/com/task01/HelloWorld.java
+++ task01/jsrc/main/java/com/task01/HelloWorld.java
@@ -20,8 +20,8 @@ public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
 	public Map<String, Object> handleRequest(Object request, Context context) {
 		System.out.println("Hello from lambda");
 		Map<String, Object> resultMap = new HashMap<String, Object>();
-		resultMap.put("message", 200);
-		resultMap.put("body", "Hello from Lambda");
+		resultMap.put("status code", 200);
+		resultMap.put("message", "Hello from Lambda");
 		return resultMap;
 	}
 }
